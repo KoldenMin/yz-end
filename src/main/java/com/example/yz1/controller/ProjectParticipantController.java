d734b2e353983a80180e81bb0f623e820e82c236
jiaohan <jiaohan01300305@gmail.com>
2025-05-06 16:23:15 +0800

    附件和图片上传功能实现

--- src/main/java/com/example/yz1/controller/ProjectParticipantController.java
+++ src/main/java/com/example/yz1/controller/ProjectParticipantController.java
@@ -8,6 +8,7 @@ import com.example.yz1.common.Result;
 import com.example.yz1.entity.Employee;
 import com.example.yz1.entity.Project;
 import com.example.yz1.entity.ProjectParticipant;
+import com.example.yz1.global.GlobalUpload;
 import com.example.yz1.mapper.ProjectParticipantMapper;
 import com.example.yz1.service.IProjectParticipantService;
 import com.example.yz1.service.IProjectService;
@@ -15,8 +16,12 @@ import com.example.yz1.vo.ProjectParticipantVO;
 import com.github.yulichang.wrapper.MPJLambdaWrapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -34,6 +39,7 @@ public class ProjectParticipantController {
     private final IProjectParticipantService projectParticipantService;
     private final ProjectParticipantMapper projectParticipantMapper;
     private final IProjectService projectService;
+    private final GlobalUpload globalUpload;
 
     // 分页
     @GetMapping("/page")
@@ -117,6 +123,51 @@ public class ProjectParticipantController {
             return Result.success();
         } else {
             return Result.error("删除失败");
+        }
+    }
+
+    // 上传附件
+    @PostMapping("/upload")
+    public Result<?> uploadFile(@RequestParam("resume") MultipartFile file,
+                                @RequestParam("projectParticipantId") Long projectParticipantId) {
+        try {
+            String resumeUrl = globalUpload.uploadResume(projectParticipantId, file, "resume", new String[]{".pdf", ".doc", ".docx"});
+
+            projectParticipantService.lambdaUpdate()
+                    .eq(ProjectParticipant::getId, projectParticipantId)
+                    .set(ProjectParticipant::getAttachmentUrl, resumeUrl)
+                    .update();
+
+            // 返回结果
+            Map<String, String> result = new HashMap<>();
+            result.put("resumeUrl", resumeUrl);
+            return Result.success(result);
+        } catch (IllegalArgumentException e) {
+            return Result.error(e.getMessage());
+        } catch (IOException e) {
+            return Result.error("简历上传失败" + e.getMessage());
+        }
+    }
+
+    // 图片上传
+    @PostMapping("/avatar")
+    public Result<?> uploadAvatar(@RequestParam("avatar") MultipartFile file,
+                                  @RequestParam("projectParticipantId") Long projectParticipantId) {
+        try {
+            if (file.isEmpty()) {
+                return Result.error("头像不能为空");
+            }
+            String avatarUrl = globalUpload.uploadAvatar(projectParticipantId, file, "avatar");
+            projectParticipantService.lambdaUpdate()
+                    .eq(ProjectParticipant::getId, projectParticipantId)
+                    .set(ProjectParticipant::getImageUrl, avatarUrl)
+                    .update();
+
+            Map<String, String> result = new HashMap<>();
+            result.put("avatarUrl", avatarUrl);
+            return Result.success(result);
+        } catch (IOException e) {
+            return Result.error("头像上传失败" + e.getMessage());
         }
     }
 
